fd578c4dcdcfc4fe709a734d717c57c00b5846de
Vivian Li <vili4418@colorado.edu>
2026-06-10 11:33:11 -0600

    get insert size sd and remove hard coded paths

--- src/data/bash/get_insert_size.sh
+++ src/data/bash/get_insert_size.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 sample_id=$1
 input_file=$2
+temp_output_dir=$3
+output_dir=$4
+samtools_exec=$5
 
-# Removed incorrect reassignment of $HOME
 CURRENT_DIR=$(pwd)
-cd /scratch/Users/vili4418/insert_size_files
+cd "$temp_output_dir"
 
-# use samtools to read the alignment file
-"$HOME/sv/samtools-1.21/samtools" view -q 5 -f 2 "$input_file" | tail -n +100000 | head -5000000 | awk 'function abs(x) { return x < 0 ? -x : x } { sum+=abs($9); count++ } END {print sum/count}' > "${sample_id}.txt"
+# use samtools to read the alignment file and calculate mean and standard deviation of insert sizes
+"$samtools_exec" view -q 5 -f 2 "$input_file" | tail -n +100000 | head -5000000 | \
+awk 'function abs(x) { return x < 0 ? -x : x } 
+  { 
+      insert_size = abs($9); 
+      sum += insert_size; 
+      sum_sq += insert_size^2; 
+      count++; 
+  } 
+  END { 
+      mean = sum / count; 
+      stddev = sqrt((sum_sq / count) - (mean^2)); 
+      print mean, stddev; 
+  }' > "${sample_id}.txt"
 
-scp "${sample_id}.txt" "${HOME}/sv/sv_gmm/1kg/insert_size_files/" 
+scp "${sample_id}.txt" "$output_dir" 
 
 cd "$CURRENT_DIR"
--- src/data/get_insert_sizes.py
+++ src/data/get_insert_sizes.py
@@ -1,32 +1,50 @@
+import argparse
 import os
 import subprocess
 import time
 
 import pandas as pd
 
+from src.utils.config_loader import load_config
 from src.utils.helper import get_sample_ids
 from src.utils.timeout import break_after
 
-FILE_DIR = "/Users/vili4418/sv/sv_gmm/1kg/insert_size_files"
-TEMP_DIR = "/scratch/Users/vili4418/insert_size_files"
 
-
-def concat_mean_insert_sizes():
-    files = os.listdir(f"{FILE_DIR}")
-    df = pd.DataFrame(columns=["sample_id", "mean_insert_size"])
+def concat_mean_insert_sizes(output_dir: str, filename: str):
+    files = os.listdir(output_dir)
+    df = pd.DataFrame(
+        columns=["sample_id", "mean_insert_size", "insert_size_sd"]
+    )
     for i, file in enumerate(files):
         sample_id = file.strip(".txt")
-        with open(f"{FILE_DIR}/{file}") as f:
+        with open(os.path.join(output_dir, file)) as f:
             mean_insert_size = int(float(f.readlines()[0].strip("\n")))
-            df.loc[i] = [sample_id, mean_insert_size]
-    df.to_csv("1kg/insert_sizes.csv", index=False)
+            insert_size_sd = int(float(f.readlines()[1].strip("\n")))
+            df.loc[i] = [sample_id, mean_insert_size, insert_size_sd]
+    df.to_csv(filename, index=False)
 
 
-@break_after(hours=5, minutes=55)
-def get_insert_sizes():
-    sample_ids = get_sample_ids()
-    df = pd.read_csv("1kg/high_cov_grch38_samples.tsv", sep="\t")
-    processed_samples = set([f.strip(".txt") for f in os.listdir(FILE_DIR)])
+@break_after(hours=11, minutes=55)
+def get_insert_sizes(
+    cfg,
+    samples_file: str,
+):
+    output_dir = cfg["paths"]["input_dir"]
+    insert_files_dir = os.path.join(output_dir, "insert_size_files")
+    os.makedirs(insert_files_dir, exist_ok=True)
+
+    df = pd.read_csv(
+        samples_file, sep="\t" if samples_file.endswith(".tsv") else ","
+    )
+    processed_samples = set(
+        [f.strip(".txt") for f in os.listdir(insert_files_dir)]
+    )
+
+    sample_ids = get_sample_ids(
+        os.path.join(output_dir, cfg["input_files"]["sample_id_file"])
+    )
+    if sample_ids is None:
+        sample_ids = df["Sample"].unique()
 
     for sample_id in sample_ids:
         if sample_id in processed_samples:
@@ -36,7 +54,14 @@ def get_insert_sizes():
         url = row["url"].values[0]
 
         subprocess.run(
-            ["bash", "bash/get_insert_size.sh"] + [sample_id, url],
+            ["bash", "bash/get_insert_size.sh"]
+            + [
+                sample_id,
+                url,
+                cfg["paths"]["intermediate_output_dir"],
+                insert_files_dir,
+                cfg["samtools"]["bin"],
+            ],
             capture_output=True,
             text=True,
         )
@@ -44,8 +69,22 @@ def get_insert_sizes():
         end = time.time()
         print(f"{sample_id} - time to get mean insert size={end - start}")
 
-    concat_mean_insert_sizes()
+    concat_mean_insert_sizes(
+        insert_files_dir,
+        os.path.join(output_dir, cfg["input_files"]["insert_size_file"]),
+    )
 
 
 if __name__ == "__main__":
-    get_insert_sizes()
+    parser = argparse.ArgumentParser(
+        description="Get the mean insert size and standard deviation for each sample."
+    )
+    parser.add_argument(
+        "--samples",
+        type=str,
+        help="Path to the file containing sample information. File must contain columns 'Sample' and 'url'",
+    )
+    args = parser.parse_args()
+
+    cfg = load_config()
+    get_insert_sizes(cfg, samples_file=args.samples)
